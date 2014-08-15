a8b10892473b0565f54b4f548cdfbef1c4385fdc
bfrasure <bill.frasure@gmail.com>
2014-08-15 14:03:11 -0600

    Bring Argonaut into project
    This is a purely functional way of dealing with JSON, which seems like
    the correct approach for me to try considering this is an early Scala
    project

--- build.sbt
+++ build.sbt
@@ -4,4 +4,8 @@ version := "1.0"
 
 scalaVersion := "2.10.2"
 
+libraryDependencies ++= Seq(
+  "io.argonaut" %% "argonaut" % "6.0.4"     
+)
+
 mainClass := Some("RepoParser")
--- src/main/scala/gitRepoParser.scala
+++ src/main/scala/gitRepoParser.scala
@@ -5,7 +5,18 @@ import scala.sys.process._
 import scala.sys.process.Process
 import com.billding.SystemCommands
 
+import argonaut._, Argonaut._
+ 
+case class Person(name: String, age: Int, things: List[String])
+ 
+
+   
+
 object RepoParser {
+
+  implicit def PersonCodecJson =
+    casecodec3(Person.apply, Person.unapply)("name", "age", "things")
+
   val git = Seq("git")
   val jsonLogger = Seq("/home/bfrasure/Repositories/Personal/scripts/gitLogJson.sh")
   val loggerArguments = Seq()
@@ -13,5 +24,18 @@ object RepoParser {
   def main(args: Array[String]) = 
   {
     println(SystemCommands.runFullCommand(jsonLogger, loggerArguments) )
+    val person =
+      Person("Bam Bam", 2, List("club"))
+
+    val json: Json =
+      person.asJson
+
+    println(json)
+
+    val prettyprinted: String =
+      json.spaces2
+
+    val parsed: Option[Person] =
+      prettyprinted.decodeOption[Person]
   }
 }
