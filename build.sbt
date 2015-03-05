4c57f35619d8c3d63a211be8ce13aa9cdd2cb862
bfrasure <bill.frasure@gmail.com>
2015-03-04 18:44:51 -0700

    Start experimenting with Actors
    I vow to figure out a case where these are helpful.

--- build.sbt
+++ build.sbt
@@ -10,7 +10,8 @@ libraryDependencies ++= Seq(
   // native libraries are not included by default. add this if you want them (as of 0.7)
   // native libraries greatly improve performance, but increase jar sizes.
   "org.scalanlp" % "breeze-natives_2.10" % "0.9",
-  "org.scalanlp" % "breeze-viz_2.10" % "0.5.1"
+  "org.scalanlp" % "breeze-viz_2.10" % "0.5.1",
+  "com.typesafe.akka" %% "akka-actor" % "2.3.9"
   )
 
 org.scalastyle.sbt.ScalastylePlugin.Settings
--- src/main/scala/gitRepoParser.scala
+++ src/main/scala/gitRepoParser.scala
@@ -13,7 +13,7 @@ import com.billding.DataWriter
 import argonaut._, Argonaut._
 
 case class LogEntry(commit: String, author: String, date: Option[String], message: Option[String])
- 
+
 object LogEntry {
   implicit def LogEntryCodecJson: CodecJson[LogEntry] =
     casecodec4(LogEntry.apply, LogEntry.unapply)("commit", "author", "date", "message")
@@ -77,62 +77,133 @@ object CommitDelta {
   implicit def stringifier(cd: CommitDelta): String = cd.toString
 }
 
-object RepoParser {
+import akka.actor.{ ActorRef, ActorSystem, Props, Actor, Inbox }
+import scala.concurrent.duration._
 
-  val home = "/home/bfrasure/"
-  val git = Seq("git")
-  val jsonLogger = Seq("/home/bfrasure/Repositories/Personal/scripts/gitLogJson.sh")
+case object Greet
+case class WhoToGreet(who: String)
+case class Greeting(message: String)
 
-  def writePlotScript(gitRepo:String, data:List[String]) = {
-    val dataWriter: DataWriter = new DataWriter
-    val utility: Utility = new Utility
+class Greeter extends Actor {
+  var greeting = ""
 
-    val plotScriptName = gitRepo.replaceAll("/","_").init
-    dataWriter.write(data, "plotfiles/"+plotScriptName+".gnuplot", utility)
-  }
-
-  def main(args: Array[String]) = 
-  {
-    val email = args(0)
-    val gitRepo = args(1)
-
-    val repoDir= home + gitRepo
-    val loggerArguments = Seq(repoDir)
-
-    val logOutput = SystemCommands.runFullCommand(loggerArguments)(jsonLogger)
-
-    val entries = logOutput.decodeOption[List[LogEntry]].getOrElse(Nil)
-
-    val userEntries = entries.filter(_.author contains email )
-    val userHashes = userEntries.map(x=>GitHash(x.commit))
-
-    val worker = new GitWorker(repoDir)
-
-    val commitDeltas: List[CommitDelta] = userHashes.zipWithIndex map {
-      case (hash,idx) => {
-        CommitDelta(
-          idx, 
-          worker.getLinesAdded( worker.showFullCommit(hash)),
-          -worker.getLinesDeleted( worker.showFullCommit(hash))
-        )
-      } 
-    }
-
-    val deltaPlots = commitDeltas.map { x => x.toString }
-
-    val dataWriter:DataWriter = new DataWriter
-    val utility:Utility = new Utility
-    // init->Return all except tail
-    val dataFileName = "data/" + gitRepo.replaceAll("/","_").init +".dat" 
-    dataWriter.write(deltaPlots, dataFileName, utility)
-
-    val plotter = new GnuPlotter
-    val plotScriptName = gitRepo.replaceAll("/","_").init
-    val plotScriptData = List(GnuPlotter.createPlotScript(plotter, plotScriptName))
-
-    writePlotScript(gitRepo, plotScriptData)
-
-    println
+  def receive = {
+    case WhoToGreet(who) => greeting = s"hello, $who"
+    case Greet           => sender ! Greeting(greeting)
   }
 }
+
+    case class RepoToExamine(url: String)
+
+    class RepoExaminer extends Actor {
+      var repo = ""
+
+      def receive = {
+        case RepoToExamine(url) => sender ! RepoData(3, "projectNameA")
+        case Greet           => sender ! RepoData(3, "projectNameA")
+      }
+    }
+
+        case class RepoData(numCommits: Int, projectName: String)
+
+        class ParserActor extends Actor {
+          def receive = {
+            case RepoData(numCommits, projectName) => println("numCommits: " + numCommits)
+          }
+        }
+
+
+
+
+        object RepoParser {
+
+          val home = "/home/bfrasure/"
+          val git = Seq("git")
+          val jsonLogger = Seq("/home/bfrasure/Repositories/Personal/scripts/gitLogJson.sh")
+
+          def writePlotScript(gitRepo:String, data:List[String]) = {
+            val dataWriter: DataWriter = new DataWriter
+            val utility: Utility = new Utility
+
+            val plotScriptName = gitRepo.replaceAll("/","_").init
+            dataWriter.write(data, "plotfiles/"+plotScriptName+".gnuplot", utility)
+          }
+
+          def actorTest(): Unit = {
+            val system = ActorSystem("helloakka")
+            val greeter = system.actorOf(Props[Greeter], "greeter")
+
+            val targetRepo = RepoToExamine("clashOfClans")
+            val parser = system.actorOf(Props[ParserActor], "parser")
+            val examiner = system.actorOf(Props[RepoExaminer], "examiner")
+
+            examiner.tell(targetRepo, parser)
+
+
+            greeter.tell(WhoToGreet("akka"), ActorRef.noSender)
+            //greeter ! WhoToGreet("akka")
+
+            // Create an "actor-in-a-box"
+            val inbox = Inbox.create(system)
+
+            // Tell the 'greeter' to change its 'greeting' message
+            greeter.tell(WhoToGreet("akka2"), ActorRef.noSender)
+
+            // Ask the 'greeter for the latest 'greeting'
+            // Reply should go to the mailbox
+            inbox.send(greeter, Greet)
+
+            // Wait 5 seconds for the reply with the 'greeting' message
+            val Greeting(message) = inbox.receive(5.seconds)
+            println(s"Greeting: $message")
+
+            system.shutdown
+            println
+          }
+
+          def main(args: Array[String]) = 
+          {
+            actorTest()
+            val email = args(0)
+            val gitRepo = args(1)
+
+            val repoDir= home + gitRepo
+            val loggerArguments = Seq(repoDir)
+
+            val logOutput = SystemCommands.runFullCommand(loggerArguments)(jsonLogger)
+
+            val entries = logOutput.decodeOption[List[LogEntry]].getOrElse(Nil)
+
+            val userEntries = entries.filter(_.author contains email )
+            val userHashes = userEntries.map(x=>GitHash(x.commit))
+
+            val worker = new GitWorker(repoDir)
+
+            val commitDeltas: List[CommitDelta] = userHashes.zipWithIndex map {
+              case (hash,idx) => {
+                CommitDelta(
+                  idx, 
+                  worker.getLinesAdded( worker.showFullCommit(hash)),
+                  -worker.getLinesDeleted( worker.showFullCommit(hash))
+                )
+              } 
+            }
+
+            val deltaPlots = commitDeltas.map { x => x.toString }
+
+            val dataWriter:DataWriter = new DataWriter
+            val utility:Utility = new Utility
+            // init->Return all except tail
+            val dataFileName = "data/" + gitRepo.replaceAll("/","_").init +".dat" 
+            dataWriter.write(deltaPlots, dataFileName, utility)
+
+            val plotter = new GnuPlotter
+            val plotScriptName = gitRepo.replaceAll("/","_").init
+            val plotScriptData = List(GnuPlotter.createPlotScript(plotter, plotScriptName))
+
+            writePlotScript(gitRepo, plotScriptData)
+
+            println
+          }
+        }
 
