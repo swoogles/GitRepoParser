f57c6063c8395d7902517dac57b70f30327b86bf
bfrasure <bill.frasure@gmail.com>
2015-03-06 23:38:18 -0700

    Move commit interactions to separate file

---
+++ src/main/scala/CommitInteraction.scala
@@ -0,0 +1,81 @@
+import util.matching.Regex
+import com.billding.SystemCommands
+
+import akka.actor.{ ActorLogging, ActorRef, ActorSystem, Props, Actor, Inbox }
+
+case class GitHash( hash: String)
+case class HashList( hashes: List[GitHash] )
+
+object CommitParser {
+  def props(repoDir: String): Props = Props(new CommitParser(repoDir))
+}
+class CommitParser(gitRepo:String) extends Actor with ActorLogging{
+  def receive = {
+    case HashList(hashes) => {
+      sender ! DataFile(gitRepo, createDeltas(hashes))
+    }
+  }
+
+  implicit val program = Seq("git")
+  val home = "/home/bfrasure/"
+  val repoDir= gitRepo + "/"
+  val gitDirectoryArguments = Seq("--git-dir=" + repoDir + ".git", "--work-tree=" + repoDir)
+
+  def getFirstNum(wordsString:String):Int = {
+    val words = wordsString split("\\s+")
+    words(0).toInt
+  }
+
+  def getNumberWithPattern(hash: GitHash, pattern: Regex ):Int = {
+    val commitInfo = showFullCommit(hash)
+    val combinedRegex = ("\\d+ " + pattern.toString).r
+    val matchingStrings = combinedRegex findAllIn commitInfo
+    if (matchingStrings nonEmpty)
+      getFirstNum(matchingStrings next) 
+    else
+      0
+  }
+
+  implicit def stringToRegex(patternString: String): Regex = {
+    patternString.r
+  }
+
+  def getFilesChanged(hash: GitHash):Int = getNumberWithPattern(hash, "files? changed")
+  def getLinesAdded(hash: GitHash):Int = getNumberWithPattern(hash, "insertions")
+  def getLinesDeleted(hash: GitHash):Int = getNumberWithPattern(hash, "deletions")
+
+  def showFullCommit(gitHash: GitHash):String = {
+    val action = "show"
+    val numStat = Seq("--numstat")
+    val shortStat = Seq("--shortstat")
+    val oneLine = Seq("--oneline")
+    SystemCommands.runFullCommand(gitDirectoryArguments++Seq(action, gitHash.hash)++shortStat)
+  }
+
+  def createDeltas(hashes: List[GitHash]): List[CommitDelta] = {
+    val commitDeltas: List[CommitDelta] = hashes.zipWithIndex map {
+      case (hash,idx) => {
+        CommitDelta(
+          idx, 
+          getLinesAdded(hash),
+          -getLinesDeleted(hash)
+        )
+      } 
+    }
+    commitDeltas
+  }
+}
+
+case class CommitDelta(idx: Long, linesAdded: Long, linesDeleted: Long)
+{
+  override def toString(): String = {
+    idx + " " + 
+    linesAdded + " " +
+    linesDeleted
+  }
+}
+object CommitDelta {
+  implicit def stringifier(cd: CommitDelta): String = cd.toString
+  implicit def multiStringifier(cds: List[CommitDelta]): List[String] = cds map { cd => cd.toString }
+}
+
--- src/main/scala/gitRepoParser.scala
+++ src/main/scala/gitRepoParser.scala
@@ -10,8 +10,6 @@ import com.billding.GnuPlotter
 import com.billding.Utility
 import com.billding.DataWriter
 
-import argonaut._, Argonaut._
-
 import akka.actor.{ ActorLogging, ActorRef, ActorSystem, Props, Actor, Inbox }
 import scala.concurrent.duration._
 
@@ -20,82 +18,6 @@ import scala.concurrent.duration._
 import scala.concurrent.Await
 import akka.pattern.ask
 
-
-case class GitHash( hash: String)
-case class HashList( hashes: List[GitHash] )
-
-object CommitParser {
-  def props(repoDir: String): Props = Props(new CommitParser(repoDir))
-}
-class CommitParser(gitRepo:String) extends Actor with ActorLogging{
-  def receive = {
-    case HashList(hashes) => {
-      sender ! DataFile(gitRepo, createDeltas(hashes))
-    }
-  }
-
-  implicit val program = Seq("git")
-  val home = "/home/bfrasure/"
-  val repoDir= gitRepo + "/"
-  val gitDirectoryArguments = Seq("--git-dir=" + repoDir + ".git", "--work-tree=" + repoDir)
-
-  def getFirstNum(wordsString:String):Int = {
-    val words = wordsString split("\\s+")
-    words(0).toInt
-  }
-
-  def getNumberWithPattern(hash: GitHash, pattern: Regex ):Int = {
-    val commitInfo = showFullCommit(hash)
-    val combinedRegex = ("\\d+ " + pattern.toString).r
-    val matchingStrings = combinedRegex findAllIn commitInfo
-    if (matchingStrings nonEmpty)
-      getFirstNum(matchingStrings next) 
-    else
-      0
-  }
-
-  implicit def stringToRegex(patternString: String): Regex = {
-    patternString.r
-  }
-
-  def getFilesChanged(hash: GitHash):Int = getNumberWithPattern(hash, "files? changed")
-  def getLinesAdded(hash: GitHash):Int = getNumberWithPattern(hash, "insertions")
-  def getLinesDeleted(hash: GitHash):Int = getNumberWithPattern(hash, "deletions")
-
-  def showFullCommit(gitHash: GitHash):String = {
-    val action = "show"
-    val numStat = Seq("--numstat")
-    val shortStat = Seq("--shortstat")
-    val oneLine = Seq("--oneline")
-    SystemCommands.runFullCommand(gitDirectoryArguments++Seq(action, gitHash.hash)++shortStat)
-  }
-
-  def createDeltas(hashes: List[GitHash]): List[CommitDelta] = {
-    val commitDeltas: List[CommitDelta] = hashes.zipWithIndex map {
-      case (hash,idx) => {
-        CommitDelta(
-          idx, 
-          getLinesAdded(hash),
-          -getLinesDeleted(hash)
-        )
-      } 
-    }
-    commitDeltas
-  }
-}
-
-case class CommitDelta(idx: Long, linesAdded: Long, linesDeleted: Long)
-{
-  override def toString(): String = {
-    idx + " " + 
-    linesAdded + " " +
-    linesDeleted
-  }
-}
-object CommitDelta {
-  implicit def stringifier(cd: CommitDelta): String = cd.toString
-  implicit def multiStringifier(cds: List[CommitDelta]): List[String] = cds map { cd => cd.toString }
-}
 
 case class PlotScript(data:List[String])
 case class DataFile(gitRepo: String, data:List[String])
