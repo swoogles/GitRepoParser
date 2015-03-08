8a936aef30b066e2f959d768c9bf4acc15eecc80
bfrasure <bill.frasure@gmail.com>
2015-03-08 12:00:39 -0600

    Move GitDispatcher to separate file

--- src/main/scala/gitRepoParser.scala
+++ src/main/scala/gitRepoParser.scala
@@ -1,69 +1,4 @@
-import com.billding.GnuPlotter
-
-import akka.actor.{ ActorLogging, ActorRef, ActorSystem, Props, Actor, Inbox }
-import scala.concurrent.duration._
-
-import akka.util.Timeout
-
-
-
-object GitDispatcher {
-  def props(filesToWrite: Int): Props = Props(new GitDispatcher(filesToWrite))
-}
-class GitDispatcher(var filesToWrite: Int) extends Actor with ActorLogging {
-  val home = "/home/bfrasure/"
-  def receive = {
-    //case DataFile(gitRepo, data) => {
-    case dataFile: DataFile => {
-      val repoFileName: String = dataFile.gitRepo.replaceAll("/","_").init
-      val dataFileCreator = context.system.actorOf(GitDataFileCreator.props(dataFile.gitRepo), repoFileName + "dataFileCreator")
-
-      //val dataForWriting = DataFile(gitRepo, data)
-      dataFileCreator ! dataFile
-      //dataFileCreator forward message
-    }
-
-    case FileWritten => {
-      //context.system.stop(sender) //TODO call this without errors
-      filesToWrite -= 1
-      println("filesToWrite left: " + filesToWrite)
-      if ( filesToWrite == 0 ) {
-        context.system.shutdown()
-        GnuPlotter.executePlotScripts()
-      }
-    }
-    case RepoTarget(gitRepo, email) => {
-      log.info("Let's get to work!")
-      val repoFileName: String = gitRepo.replaceAll("/","_").init
-      val repoDir= home + gitRepo + "/"
-      val jsonLogger = new JsonLogger(repoDir)
-
-      val entries = jsonLogger.repoLogs()
-
-      val userEntries = entries.filter(_.author contains email )
-
-      val userHashes = userEntries.map(x=>GitHash(x.commit))
-
-      val commitParser = context.system.actorOf(CommitParser.props(gitRepo), repoFileName + "commitParser")
-
-      implicit val timeout = Timeout(5 seconds)
-      //commitParser ! HashList(userHashes)
-
-      val plotFileCreator = context.system.actorOf(GitDataFileCreator.props(gitRepo), repoFileName + "plotFileCreator")
-
-      // After parser does its work, it should tell the results to dataFileCreator
-      // I'm sure there's a more proper way where dataFileCreator is already the
-      // sender, but this will have to do for now.
-      commitParser ! HashList(userHashes)
-
-      val plotter = new GnuPlotter
-      val plotScriptName = repoFileName
-      val plotScriptData = List(plotter.createPlotScript(plotScriptName))
-
-      plotFileCreator ! PlotScript(plotScriptData)
-    }
-  }
-}
+import akka.actor.{ ActorSystem, Actor}
 
 object GitManager {
   val home = "/home/bfrasure/"
@@ -102,7 +37,6 @@ object GitManager {
     //Thread.sleep(8000)
     //system.shutdown
 
-    //GnuPlotter.executePlotScripts()
     println
   }
 }
