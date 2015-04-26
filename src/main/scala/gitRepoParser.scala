67d16041b20d6b071aec49a5819165bee682f38e
bfrasure <bill.frasure@gmail.com>
2015-04-26 14:22:53 -0600

    Enable Action choice via cmd line param

--- src/main/scala/gitRepoParser.scala
+++ src/main/scala/gitRepoParser.scala
@@ -12,32 +12,49 @@ object GitManager {
 
   def main(args: Array[String]) = 
   {
-    val email = args(0)
+    val email = args(0).split("\\s+")(0)
+    println(s"email: ${email}")
+    val action = args(0).split("\\s+")(1)
+    println(s"action: ${action}") 
+
+    val availableActions = Map(
+      "filesChanged" -> FilesChanged,
+    "LineDeltas" -> LineDeltas
+  )
+
+    val chosenAction: Option[CommitAction] = availableActions.get(action)
+    println(s"chosenAction: ${chosenAction}")
 
     val repos = List(
       //"AtomicScala",
-      //"AudioHand/Mixer",
-      "ClashOfClans",
-      "ConcurrencyInAction",
-      "GitRepoParser",
-      "Latex",
-      //"Personal",
-      //"Physics",
-      //"ProjectEuler",
-      "RoundToNearestX"
-    ) map {  x => Repo("Repositories/" + x , home) }
+    //"AudioHand/Mixer",
+    "ClashOfClans",
+    "ConcurrencyInAction",
+    "GitRepoParser",
+    "Latex",
+    //"Personal",
+    //"Physics",
+    //"ProjectEuler",
+    "RoundToNearestX"
+  ) map {  x => Repo("Repositories/" + x , home) }
 
-    val system = ActorSystem("helloakka")
-    val numRepos = repos.size
-    val filesPerRepo = 2
-    val filesToWrite = numRepos * filesPerRepo
-    val dispatcher = system.actorOf(GitDispatcher.props(filesToWrite), "dispatcher")
 
     //val commitAction: CommitAction = FilesChanged
-    val commitAction: CommitAction = LineDeltas
 
-    for { repo <- repos } 
-      { dispatcher ! RepoAndAction(repo, commitAction) }
+    chosenAction match {
+      case Some(commitAction) => {
+        val system = ActorSystem("helloakka")
+        val numRepos = repos.size
+        val filesPerRepo = 2
+        val filesToWrite = numRepos * filesPerRepo
+        val dispatcher = system.actorOf(GitDispatcher.props(filesToWrite), "dispatcher")
+        for { repo <- repos } 
+        { dispatcher ! RepoAndAction(repo, commitAction) }
+      }
+      case None =>  {
+        println("Bad Action. I'm not taking another step.")
+      }
+    }
 
   }
 }
